8b8f136d28518e28abd01407f5230d4f80039121
Shuta Kimura <shuta_kimura@omron.co.jp>
2012-10-30 19:02:28 +0900

    calcを実装した。
    明日はload,printの簡易テスト作成と、本体の実装。

--- coins.rb
+++ coins.rb
@@ -1,10 +1,26 @@
 #!/usr/bin/env ruby
 
 class Coins
+	KINDS = [500, 100, 50, 10, 5, 1]
 	# def hoge
 	# end
 	def load; end
-	def calc(counts, amount); end
+	def calc(counts, amount)
+		KINDS.each { |kind|
+			raise "not positive number" if counts[kind] < 0
+		}
+
+		answer = { 'sum' => 0 }
+		KINDS.each { |kind|
+			draw = [amount / kind, counts[kind]].min
+
+			answer[kind] = draw
+			answer['sum'] += draw
+			amount -= draw * kind
+		}
+		raise "cannot pay" if amount != 0
+		answer
+	end
 	def print; end
 end
 
--- coins_spec.rb
+++ coins_spec.rb
@@ -7,8 +7,49 @@ describe Coins do
 	it 'クラスがnewできること' do
 		Coins.new.should_not be_nil
 	end
-	it 'ほげほげ' do
-		Coins.new.load.should be_nil
+
+	it 'calc 入力チェック' do
+		input_count = { '500' => nil,
+					'100' => 10,
+					'50' => 10,
+					'10' => 10,
+					'5' => 10,
+					'1' => 10 }
+		input_amount = 10
+		proc {
+			Coins.new.calc(input_count, input_amount)
+		}.should raise_error
 	end
+	
+	it 'calc 答えを出す' do
+		input_count = { 500 => 1,
+					100 => 4,
+					50 => 1,
+					10 => 4,
+					5 => 1,
+					1 => 6 }
+		input_amount = 1000
+		Coins.new.calc(input_count, input_amount).should == { 500 => 1,
+			100 => 4,
+			50 => 1,
+			10 => 4,
+			5 => 1,
+			1 => 5,
+			'sum' => 16 }
+	end
+	
+	it 'calc 答えがない場合' do
+		input_count = { 500 => 1,
+					100 => 4,
+					50 => 1,
+					10 => 4,
+					5 => 1,
+					1 => 6 }
+		input_amount = 10000
+		proc {
+			Coins.new.calc(input_count, input_amount)
+		}.should raise_error
+	end
+	
 end
 
